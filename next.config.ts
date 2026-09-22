2b6893366b3037d6cda9f2fe43d5fd8f2ac51376
craj68168 <craj68168@gmail.com>
2026-09-23 00:05:58 +0545

    feat:next config updated

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   trailingSlash: true,
   output: "export",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
